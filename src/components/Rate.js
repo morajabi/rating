a127b5e40544d5b1281f73d8d6023e140e1e0432
Mohammad Rajabifard <mo.rajbi@gmail.com>
2018-01-13 21:54:11 +0330

    feat: make radio buttons clickable, focusable, WAI-ARIA compliment and hoverable

--- src/components/Rate.js
+++ src/components/Rate.js
@@ -1,23 +1,92 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
-import { v4 } from 'uuid'
 
 import StarRadio from './StarRadio'
 
 class Rate extends Component {
-  renderRadios() {
-    let radios = []
-    for (let i = 1; i <= 10; i++) {
-      radios.push(<StarRadio id={v4()} number={i} checked={false} />)
+  constructor(p) {
+    super(p)
+    this.numbers = this.generateNumbersArray()
+    this.state = {
+      // Check the middle star as default
+      checked: 5,
+      active: 5,
     }
-    return radios
+  }
+
+  generateNumbersArray() {
+    let numbers = []
+    for (let i = 1; i <= 10; i++) {
+      numbers.push(i)
+    }
+    return numbers
   }
 
   render() {
-    return <Wrapper>{this.renderRadios()}</Wrapper>
+    return (
+      <Wrapper onSubmit={this.submitted}>
+        <div role="radiogroup">
+          {this.numbers.map(num => (
+            <StarRadio
+              name="rate"
+              id={`rate-radio-${num}`}
+              number={num}
+              checked={this.state.checked === num}
+              active={this.state.active && this.state.active >= num}
+              onClick={this.onRadioClick}
+              onMouseEnter={this.onRadioMouseEnter}
+              onMouseLeave={this.onRadioMouseLeave}
+              onInputChange={this.onRadioInputChange}
+            />
+          ))}
+        </div>
+        <SubmitButton type="submit" aria-hidden="true" tabIndex="-1" />
+      </Wrapper>
+    )
+  }
+
+  setCheckedAndActive = num => {
+    this.setState({ checked: num, active: num })
+  }
+
+  onRadioClick = (num, inputRef) => {
+    this.setCheckedAndActive(num)
+    if (inputRef) {
+      inputRef.focus()
+    }
+    // Submit form
+    this.submitted()
+  }
+
+  onRadioMouseEnter = num => {
+    this.setState({ active: num })
+  }
+
+  onRadioMouseLeave = () => {
+    this.setState(prev => ({ active: prev.checked }))
+  }
+
+  onRadioInputChange = e => {
+    // This event is triggered from radio input (wit keyboard or screenreader)
+    this.setCheckedAndActive(Number(e.target.value))
+  }
+
+  submitted = e => {
+    if (e) {
+      e.preventDefault()
+    }
+
+    console.log('Form Submitted!')
+
+    return false
   }
 }
 
 export default Rate
 
-const Wrapper = styled.div``
+const Wrapper = styled.form``
+const SubmitButton = styled.input`
+  position: absolute;
+  left: -9999px;
+  opacity: 0;
+`
--- src/components/StarRadio.js
+++ src/components/StarRadio.js
@@ -1,37 +1,80 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
 import { rem } from '../utils/rem'
 import { StarDefault, StarActive } from '../components/Svgs'
 
-const StarRadio = ({ number, checked, id }) => (
-  <Wrapper id={id} role="checkbox" aria-checked={String(checked)}>
-    <Label>
-      <Icon>
-        <StarActive />
-      </Icon>
-      <NumberText>{number}</NumberText>
-    </Label>
-  </Wrapper>
-)
+class StarRadio extends Component {
+  constructor(p) {
+    super(p)
+    this.inputRef = null
+  }
+
+  render() {
+    const {
+      name,
+      number,
+      checked,
+      active,
+      id,
+      onInputChange = () => {},
+      onClick = () => {},
+      onMouseEnter = () => {},
+      onMouseLeave = () => {},
+    } = this.props
+
+    return (
+      <Wrapper>
+        <RadioInput
+          id={id}
+          type="radio"
+          name={name}
+          value={number}
+          checked={checked}
+          aria-checked={String(checked)}
+          innerRef={c => {
+            this.inputRef = c
+          }}
+          onChange={onInputChange}
+        />
+        <Label
+          for={id}
+          onClick={() => onClick(number, this.inputRef)}
+          onMouseEnter={() => onMouseEnter(number)}
+          onMouseLeave={() => onMouseLeave(number)}
+        >
+          <Icon>{active ? <StarActive /> : <StarDefault />}</Icon>
+          <NumberText>{number}</NumberText>
+        </Label>
+      </Wrapper>
+    )
+  }
+}
 
 export default StarRadio
 
 StarRadio.propTypes = {
   id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
   number: PropTypes.number.isRequired,
   checked: PropTypes.bool.isRequired,
+  active: PropTypes.bool.isRequired,
 }
 
 const Wrapper = styled.span`
   display: inline-block;
+  position: relative;
 `
 
 const Label = styled.label`
   display: inline-block;
   padding: 0 5px;
   cursor: pointer;
+
+  &:focus {
+    background: blue;
+  }
 `
 
 const Icon = styled.span`
@@ -43,4 +86,15 @@ const NumberText = styled.span`
   margin-top: 9px;
   font-size: ${rem(12)};
   color: #999;
+`
+
+const RadioInput = styled.input`
+  position: absolute;
+  appearance: none;
+  opacity: 0;
+
+  &:focus + ${Label} {
+    border-radius: 5px;
+    box-shadow: 0 0 0 2px lightBlue;
+  }
 `
